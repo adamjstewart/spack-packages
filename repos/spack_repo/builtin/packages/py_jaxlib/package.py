013dbbda88bd0ac265aaadce6ec500978411a1d5
Adam J. Stewart <ajstewart426@gmail.com>
2026-09-22 11:28:03 +0200

    JAX: add v0.11.2
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- repos/spack_repo/builtin/packages/py_jaxlib/package.py
+++ repos/spack_repo/builtin/packages/py_jaxlib/package.py
@@ -49,6 +49,7 @@ class PyJaxlib(PythonPackage, CudaPackage, ROCmPackage):
     license("Apache-2.0")
     maintainers("adamjstewart", "jonas-eschle", "afzpatel")
 
+    version("0.11.2", sha256="12dd77d86ec5a316be6038c9a597a1479bce13c828722a249b022e774b2eff75")
     version("0.11.1", sha256="ef9826243bcb8eae6d39ac71580bb39154fbeca51b40c2371aab5db1a797dfb4")
     version("0.11.0", sha256="007ef373573ff2fb8a5485679b791581fda328754fd7ae491de3bcdb0fc70d07")
     version("0.10.2", sha256="fa7214ab31ed1cd418b4305807e9c4f3f175c783eeea40c28e0f77c3f4c24bc7")
@@ -106,6 +107,7 @@ class PyJaxlib(PythonPackage, CudaPackage, ROCmPackage):
         depends_on("cuda@12.1:", when="@0.4.26:")
         depends_on("cuda@11.8:", when="@0.4.11:")
         depends_on("cuda@11.4:", when="@0.4.0:0.4.7")
+        depends_on("cudnn@9.10.2:9", when="@0.11.2:")
         depends_on("cudnn@9.8:9", when="@0.7.1:")
         depends_on("cudnn@9.1:9", when="@0.4.31:0.7.0")
         depends_on("cudnn@9", when="@0.4.29:0.4.30")
@@ -139,7 +141,8 @@ class PyJaxlib(PythonPackage, CudaPackage, ROCmPackage):
 
         # Bazel tends to be backwards-compatible within major versions
         # .bazelversion
-        depends_on("bazel@7.7.1:7", when="@0.11.1:")
+        depends_on("bazel@8.7.0:8", when="@0.11.2:")
+        depends_on("bazel@7.7.1:7", when="@0.11.1")
         depends_on("bazel@7.7.0:7", when="@0.8.1:0.11.0")
         depends_on("bazel@7.4.1:7", when="@0.5.3:0.8.0")
         depends_on("bazel@6.5.0:6", when="@0.4.28:0.5.2")
